8ecce79cea150803b34aa9e39121b27a2901196f
Jeffrey Yu <jwy5140@gmail.com>
2019-02-19 03:11:09 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,5 +15,5 @@
  def speak_to_grandma(str)
    if (str == "I LOVE YOU GRANDMA!")
      puts "I LOVE YOU TOO PUMPKIN!"
-    else if (str == str.upcase)
+    elsif (str == str.upcase)
      puts "NO, NOT SINCE 1938!"
